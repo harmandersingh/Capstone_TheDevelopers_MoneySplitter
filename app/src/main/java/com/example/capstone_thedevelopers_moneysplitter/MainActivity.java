58477fd62a2aa1891624b6e7d5cc6b744332bd49
Jimmy <harmander.singh.rai@gmail.com>
2021-04-14 22:47:12 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/capstone_thedevelopers_moneysplitter/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.capstone_thedevelopers_moneysplitter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
