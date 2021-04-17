daa068f3341e2111b91fdc5d8cfdf193969a6eba
Jimmy <harmander.singh.rai@gmail.com>
2021-04-17 23:31:56 +0530

    expanse list add

---
+++ app/src/main/java/com/example/capstone_thedevelopers_moneysplitter/ExpanseListFragment.java
@@ -0,0 +1,18 @@
+package com.example.capstone_thedevelopers_moneysplitter;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+
+public class ExpanseListFragment extends Fragment {
+
+}
