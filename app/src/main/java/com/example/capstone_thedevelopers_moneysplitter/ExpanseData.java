67ebde8a69a95de935f2466d4a1607fb66a36c59
Jimmy <harmander.singh.rai@gmail.com>
2021-04-17 23:11:02 +0530

    Expanse data

---
+++ app/src/main/java/com/example/capstone_thedevelopers_moneysplitter/ExpanseData.java
@@ -0,0 +1,79 @@
+package com.example.capstone_thedevelopers_moneysplitter;
+
+import java.io.Serializable;
+
+public class ExpanseData implements Serializable {
+    String userId;
+    String userName;
+    String expanse;
+    String image;
+    String description;
+    String lat;
+    String lng;
+    String address;
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getExpanse() {
+        return expanse;
+    }
+
+    public void setExpanse(String expanse) {
+        this.expanse = expanse;
+    }
+
+    public String getLat() {
+        return lat;
+    }
+
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+
+    public String getLng() {
+        return lng;
+    }
+
+    public void setLng(String lng) {
+        this.lng = lng;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
+
