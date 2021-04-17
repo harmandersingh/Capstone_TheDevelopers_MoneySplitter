b89e155095e6c12c9b431821df8ac4a71301b70c
Jimmy <harmander.singh.rai@gmail.com>
2021-04-17 22:54:59 +0530

    phone verification update

--- app/src/main/java/com/example/capstone_thedevelopers_moneysplitter/PhoneNumberVerification.java
+++ app/src/main/java/com/example/capstone_thedevelopers_moneysplitter/PhoneNumberVerification.java
@@ -28,6 +28,87 @@ import java.io.Serializable;
 import java.util.concurrent.TimeUnit;
 
 public class PhoneNumberVerificationActivity extends AppCompatActivity {
+    TextView btnVerify;
+    ProgressBar progressBar;
+    boolean isVerifyOtp = false;
+    EditText edtPhone, edtOtp;
+    // string for storing our verification ID
+    private String verificationId;
+    // [START declare_auth]
+    private FirebaseAuth mAuth;
+
+    private PhoneAuthProvider.ForceResendingToken mResendToken;
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
 
 
-}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phone_number_verification);
+        btnVerify = findViewById(R.id.btnVerify);
+        edtPhone = findViewById(R.id.edtPhone);
+        edtOtp = findViewById(R.id.edtOtp);
+        progressBar = findViewById(R.id.progressBar);
+        mAuth = FirebaseAuth.getInstance();
+
+
+        mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+            @Override
+            public void onVerificationCompleted(PhoneAuthCredential credential) {
+                // This callback will be invoked in two situations:
+                // 1 - Instant verification. In some cases the phone number can be instantly
+                //     verified without needing to send or enter a verification code.
+                // 2 - Auto-retrieval. On some devices Google Play services can automatically
+                //     detect the incoming verification SMS and perform verification without
+                //     user action.
+                final String code = credential.getSmsCode();
+
+                // checking if the code
+                // is null or not.
+                if (code != null) {
+                    // if the code is not null then
+                    // we are setting that code to
+                    // our OTP edittext field.
+                    edtOtp.setText(code);
+
+                    // after setting this code
+                    // to OTP edittext field we
+                    // are calling our verifycode method.
+                    verifyCode(code);
+                }
+            }
+
+            @Override
+            public void onVerificationFailed(FirebaseException e) {
+                // This callback is invoked in an invalid request for verification is made,
+                // for instance if the the phone number format is not valid.
+                Log.w("TAG", "onVerificationFailed", e);
+
+                if (e instanceof FirebaseAuthInvalidCredentialsException) {
+                    // Invalid request
+                } else if (e instanceof FirebaseTooManyRequestsException) {
+                    // The SMS quota for the project has been exceeded
+                }
+
+                // Show a message and update the UI
+            }
+
+            @Override
+            public void onCodeSent(@NonNull String vId,
+                                   @NonNull PhoneAuthProvider.ForceResendingToken token) {
+                // The SMS verification code has been sent to the provided phone number, we
+                // now need to ask the user to enter the code and then construct a credential
+                // by combining the code with a verification ID.
+                Log.d("TAG", "onCodeSent:" + vId);
+
+                // Save verification ID and resending token so we can use them later
+                verificationId = vId;
+                mResendToken = token;
+                progressBar.setVisibility(View.GONE);
+            }
+        };
+
+
+
+    }
