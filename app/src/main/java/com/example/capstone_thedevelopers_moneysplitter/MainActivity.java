9d7e4268a4eda425e290cf1318cb80b3c208ef8c
Jimmy <harmander.singh.rai@gmail.com>
2021-04-17 22:48:35 +0530

    main activity

--- app/src/main/java/com/example/capstone_thedevelopers_moneysplitter/MainActivity.java
+++ app/src/main/java/com/example/capstone_thedevelopers_moneysplitter/MainActivity.java
@@ -1,14 +1,77 @@
 package com.example.capstone_thedevelopers_moneysplitter;
 
+iimport androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    LocationManager locationManager;
+    public String latitude = "0", longitude = "0";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        openFragment(new TripOptionFragment(), false);
+        getCurrentLocation();
     }
+
+    public void openFragment(Fragment fragment, boolean addToBackStack) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        if (addToBackStack) {
+            transaction.addToBackStack(null);
+        }
+        transaction.commit();
+    }
+
+
+    public   void getCurrentLocation() {
+
+        try {
+            if (ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED &&
+                    ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION)
+                            != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 101);
+            }else{
+                locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+//                List<String> providers = locationManager.getProviders(true);
+                Location locationGPS = locationManager.getLastKnownLocation("gps");
+                if (locationGPS != null) {
+                    double lat = locationGPS.getLatitude();
+                    double longi = locationGPS.getLongitude();
+                    latitude = String.valueOf(lat);
+                    longitude = String.valueOf(longi);
+                    //  showLocation.setText("Your Location: " + "\n" + "Latitude: " + latitude + "\n" + "Longitude: " + longitude);
+                } else {
+                    Toast.makeText(this, "Unable to find location.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void setUpLocationListener() {
+
+
+
+    }
+
 }
