23b50e92e45e4f1611c8d43c0c88836a9447a68a
Jimmy <harmander.singh.rai@gmail.com>
2021-04-17 23:33:06 +0530

    expanse list update

--- app/src/main/java/com/example/capstone_thedevelopers_moneysplitter/ExpanseListFragment.java
+++ app/src/main/java/com/example/capstone_thedevelopers_moneysplitter/ExpanseListFragment.java
@@ -14,5 +14,44 @@ import java.util.ArrayList;
 
 
 public class ExpanseListFragment extends Fragment {
+    RecyclerView rvExpanse;
+    ArrayList<ExpanseData> expanseDataArrayList = new ArrayList<>();
+
+    public ExpanseListFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_expanse_list, container, false);
+        expanseDataArrayList = (ArrayList<ExpanseData>) getArguments().getSerializable("dataList");
+        rvExpanse = view.findViewById(R.id.rvExpanse);
+        setAdapter();
+        return view;
+    }
+
+    void setAdapter() {
+        rvExpanse.setLayoutManager(new LinearLayoutManager(requireContext()));
+        ExpanseAdapter adapter = new ExpanseAdapter(requireContext(), expanseDataArrayList, index -> {
+
+            Intent intent = new Intent(getActivity(), ExpansedetailActivity.class);
+            intent.putExtra("data", expanseDataArrayList.get(index));
+            startActivity(intent);
+//            ExpenseDetailFragment expenseDetailFragment = new ExpenseDetailFragment();
+//            expenseDetailFragment.setArguments(bundle);
+//            ((MainActivity) getActivity()).openFragment(expenseDetailFragment, true);
+        });
+        rvExpanse.setAdapter(adapter);
+    }
 
 }
