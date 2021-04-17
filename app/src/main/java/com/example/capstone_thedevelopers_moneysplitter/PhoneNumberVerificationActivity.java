1e90bb15e5a88ee33de28ffb83b60dd69ecdb04f
Jimmy <harmander.singh.rai@gmail.com>
2021-04-17 23:05:08 +0530

    Phone Verification new

---
+++ app/src/main/java/com/example/capstone_thedevelopers_moneysplitter/PhoneNumberVerificationActivity.java
@@ -0,0 +1,32 @@
+package com.example.capstone_thedevelopers_moneysplitter;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.TaskExecutors;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.FirebaseTooManyRequestsException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.io.Serializable;
+import java.util.concurrent.TimeUnit;
+
+public class PhoneNumberVerificationActivity extends AppCompatActivity {
+
+}
