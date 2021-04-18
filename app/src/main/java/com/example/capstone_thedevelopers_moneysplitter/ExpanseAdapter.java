aa839598ab580becb97e4a277a34e1d022c49639
DELL <raj749715@gmail.com>
2021-04-18 13:14:06 -0400

    update Expanse Adapter file

--- app/src/main/java/com/example/capstone_thedevelopers_moneysplitter/ExpanseAdapter.java
+++ app/src/main/java/com/example/capstone_thedevelopers_moneysplitter/ExpanseAdapter.java
@@ -11,8 +11,22 @@ import android.widget.TextView;
 
 
 public class TripOptionFragment extends Fragment {
+    TextView btnAddTrip,btnViewTrip;
+
     public TripOptionFragment() {
         // Required empty public constructor
     }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_trip_option, container, false);
+        // Inflate the layout for this fragment
+        btnAddTrip = view.findViewById(R.id.btnAddTrip);
+        btnViewTrip = view.findViewById(R.id.btnViewTrip);
 
+        btnAddTrip.setOnClickListener(v -> ((MainActivity)getActivity()).openFragment(new AddTripFragment(),true));
+        btnViewTrip.setOnClickListener(v -> ((MainActivity)getActivity()).openFragment(new ViewTripFragment(),true));
+
+        return view;
+    }
 }
