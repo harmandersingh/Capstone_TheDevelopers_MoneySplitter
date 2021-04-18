65849040f6f1f6f7228a5d9c9a59d76f43d9d6a7
DELL <raj749715@gmail.com>
2021-04-18 13:39:28 -0400

    update Expanse detail file

--- app/src/main/java/com/example/capstone_thedevelopers_moneysplitter/ExpanseDetailFragment.java
+++ app/src/main/java/com/example/capstone_thedevelopers_moneysplitter/ExpanseDetailFragment.java
@@ -19,4 +19,50 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 
 public class ExpanseDetailFragment extends Fragment implements OnMapReadyCallback {
+    private GoogleMap mMap;
+    ExpanseData expanseData = new ExpanseData();
+
+    public ExpenseDetailFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_expense_detail, container, false);
+        // Inflate the layout for this fragment
+        expanseData = (ExpanseData) getArguments().getSerializable("data");
+        SupportMapFragment mapFragment = (SupportMapFragment) getActivity().getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        long lat = Long.parseLong(expanseData.lat);
+        long lng = Long.parseLong(expanseData.lng);
+
+        mMap = googleMap;
+        // Add a marker in Sydney and move the camera
+        LatLng TutorialsPoint = new LatLng(lat, lng);
+        mMap.addMarker(new
+                MarkerOptions().position(TutorialsPoint).title(""));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(TutorialsPoint));
+    }
 }
