02087adeb6a4f47ba1517478ce903cefdd0c7c4e
DELL <raj749715@gmail.com>
2021-04-18 13:54:24 -0400

    update expense details file

--- app/src/main/java/com/example/capstone_thedevelopers_moneysplitter/ExpanseDetailActivity.java
+++ app/src/main/java/com/example/capstone_thedevelopers_moneysplitter/ExpanseDetailActivity.java
@@ -13,4 +13,29 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class ExpanseDetailActivity extends AppCompatActivity implements OnMapReadyCallback {
-}
+    private GoogleMap mMap;
+    ExpanseData expanseData = new ExpanseData();
+    private long lat,lng;
+    TextView txtName,txtAmount,txtDescription;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_expansedetail);
+        txtName = findViewById(R.id.txtName);
+        txtAmount = findViewById(R.id.txtAmount);
+        txtDescription = findViewById(R.id.txtDescription);
+        expanseData = (ExpanseData) getIntent().getSerializableExtra("data");
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        txtName.setText(expanseData.getUserName());
+        txtAmount.setText(expanseData.getExpanse());
+        txtDescription.setText(expanseData.getDescription());
+//       lat = Long.parseLong(expanseData.getLat());
+//        lng = Long.parseLong(expanseData.getLng());
+
+    }
+
+   }
+
