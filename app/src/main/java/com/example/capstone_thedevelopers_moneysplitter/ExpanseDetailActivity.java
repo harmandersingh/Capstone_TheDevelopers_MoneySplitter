810f496fd69adc07342221a9463318397b9c563b
DELL <raj749715@gmail.com>
2021-04-18 13:50:23 -0400

    add expense details file

--- app/src/main/java/com/example/capstone_thedevelopers_moneysplitter/ExpanseDetailActivity.java
+++ app/src/main/java/com/example/capstone_thedevelopers_moneysplitter/ExpanseDetailActivity.java
@@ -1,4 +1,16 @@
 package com.example.capstone_thedevelopers_moneysplitter;
 
-public class ExpanseDetailActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class ExpanseDetailActivity extends AppCompatActivity implements OnMapReadyCallback {
 }
