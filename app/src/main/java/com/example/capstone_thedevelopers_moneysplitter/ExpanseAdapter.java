bec1c4904738dc92294d2ed361d7c400a4995b53
DELL <raj749715@gmail.com>
2021-04-18 13:12:10 -0400

    update Expanse Adapter file

--- app/src/main/java/com/example/capstone_thedevelopers_moneysplitter/ExpanseAdapter.java
+++ app/src/main/java/com/example/capstone_thedevelopers_moneysplitter/ExpanseAdapter.java
@@ -1,4 +1,18 @@
 package com.example.capstone_thedevelopers_moneysplitter;
 
-public class ExpanseAdapter {
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class TripOptionFragment extends Fragment {
+    public TripOptionFragment() {
+        // Required empty public constructor
+    }
+
 }
