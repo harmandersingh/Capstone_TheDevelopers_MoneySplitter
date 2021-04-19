dbb6924bd2ee302a255484f56421dbdeb70ca65a
Love98729 <77941725+Love98729@users.noreply.github.com>
2021-04-19 19:50:18 +0530

    update MemberAdapter

--- app/src/main/java/com/example/capstone_thedevelopers_moneysplitter/MemberAdapter.java
+++ app/src/main/java/com/example/capstone_thedevelopers_moneysplitter/MemberAdapter.java
@@ -1,4 +1,10 @@
 package com.example.capstone_thedevelopers_moneysplitter;
 
+import android.view.LayoutInflater;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
 public class MemberAdapter {
 }
