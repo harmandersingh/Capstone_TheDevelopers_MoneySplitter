35946544dd16ca5ad7e23d5ab3c4483ca67bf0c6
Love98729 <77941725+Love98729@users.noreply.github.com>
2021-04-19 19:55:27 +0530

    update MemberAdapter

--- app/src/main/java/com/example/capstone_thedevelopers_moneysplitter/MemberAdapter.java
+++ app/src/main/java/com/example/capstone_thedevelopers_moneysplitter/MemberAdapter.java
@@ -1,10 +1,27 @@
 package com.example.capstone_thedevelopers_moneysplitter;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 
+import androidx.recyclerview.widget.RecyclerView;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
-public class MemberAdapter {
-}
+public class MembersAdapter extends RecyclerView.Adapter<MembersAdapter.ViewHolder> {
+
+    private ArrayList<UserData> mData;
+    private LayoutInflater mInflater;
+
+    // data is passed into the constructor
+    MembersAdapter(Context context, ArrayList<UserData> data) {
+        this.mInflater = LayoutInflater.from(context);
+        this.mData = data;
+    }
