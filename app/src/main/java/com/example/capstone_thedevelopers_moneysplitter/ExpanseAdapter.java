085f20b1c642686f646a9a6ba06900f11d17c594
DELL <raj749715@gmail.com>
2021-04-20 14:25:05 -0400

    add expanse apapter file

--- app/src/main/java/com/example/capstone_thedevelopers_moneysplitter/ExpanseAdapter.java
+++ app/src/main/java/com/example/capstone_thedevelopers_moneysplitter/ExpanseAdapter.java
@@ -1,32 +1,4 @@
 package com.example.capstone_thedevelopers_moneysplitter;
 
-import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-
-
-public class TripOptionFragment extends Fragment {
-    TextView btnAddTrip,btnViewTrip;
-
-    public TripOptionFragment() {
-        // Required empty public constructor
-    }
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_trip_option, container, false);
-        // Inflate the layout for this fragment
-        btnAddTrip = view.findViewById(R.id.btnAddTrip);
-        btnViewTrip = view.findViewById(R.id.btnViewTrip);
-
-        btnAddTrip.setOnClickListener(v -> ((MainActivity)getActivity()).openFragment(new AddTripFragment(),true));
-        btnViewTrip.setOnClickListener(v -> ((MainActivity)getActivity()).openFragment(new ViewTripFragment(),true));
-
-        return view;
-    }
+public class ExpanseAdapter {
 }
