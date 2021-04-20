11d3ef015351166a0496b219ea260150500f6e77
Love98729 <77941725+Love98729@users.noreply.github.com>
2021-04-21 02:15:05 +0530

    Update

--- app/src/main/java/com/example/capstone_thedevelopers_moneysplitter/ExpanseListFragment.java
+++ app/src/main/java/com/example/capstone_thedevelopers_moneysplitter/ExpanseListFragment.java
@@ -14,8 +14,14 @@ import java.util.ArrayList;
 
 
 public class ExpanseListFragment extends Fragment {
+    TextView txtEndTrip, txtPay;
     RecyclerView rvExpanse;
     ArrayList<ExpanseData> expanseDataArrayList = new ArrayList<>();
+    ArrayList<UserData> userDataArrayList = new ArrayList<>();
+    private FirebaseDatabase mFirebaseInstance;
+    SharedPreferences mPrefs;
+    private UserData userData = new UserData();
+    String totalSpend;
 
     public ExpanseListFragment() {
         // Required empty public constructor
@@ -35,9 +41,73 @@ public class ExpanseListFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_expanse_list, container, false);
         expanseDataArrayList = (ArrayList<ExpanseData>) getArguments().getSerializable("dataList");
+        userDataArrayList = (ArrayList<UserData>) getArguments().getSerializable("userData");
+        totalSpend = getArguments().getString("totalSpend");
         rvExpanse = view.findViewById(R.id.rvExpanse);
+        txtEndTrip = view.findViewById(R.id.txtEndTrip);
+        txtPay = view.findViewById(R.id.txtPay);
+        mFirebaseInstance = FirebaseDatabase.getInstance();
+        mPrefs = getActivity().getSharedPreferences("MyPREFERENCES", Context.MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = mPrefs.getString("userData", "");
+        userData = gson.fromJson(json, UserData.class);
+        for (int i = 0; i < userDataArrayList.size(); i++) {
+            int totalAmount = 0;
+            for (int j = 0; j < expanseDataArrayList.size(); j++) {
+                if (userDataArrayList.get(i).getUserId().equals(expanseDataArrayList.get(j).getUserId())) {
+                    totalAmount = totalAmount + Integer.parseInt(expanseDataArrayList.get(j).getExpanse());
+                }
+            }
+            userDataArrayList.get(i).setCurrentTripTotalExpanse(String.valueOf(totalAmount));
+        }
+        txtEndTrip.setOnClickListener(v -> {
+            showDialog();
+        });
+        txtPay.setOnClickListener(v -> {
+
+        });
         setAdapter();
         return view;
+    }
+
+    private void showDialog() {
+        final Dialog dialog = new Dialog(getActivity());
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.dialog_end_trip);
+        TextView txtEndTrip = (TextView) dialog.findViewById(R.id.txtEndTrip);
+        TextView perHead = (TextView) dialog.findViewById(R.id.perHead);
+        int split = Integer.parseInt(totalSpend) / userDataArrayList.size();
+        perHead.setText( "$ " +split );
+        RecyclerView rvPayments = (RecyclerView) dialog.findViewById(R.id.rvPayments);
+        setPaymentAdapter(rvPayments);
+        txtEndTrip.setOnClickListener(v -> {
+            clearTripId();
+            dialog.dismiss();
+        });
+        dialog.show();
+    }
+    private void clearTripId() {
+        for (int i = 0; i < userDataArrayList.size(); i++) {
+            mFirebaseInstance.getReference("Users").child(userDataArrayList.get(i).getUserId()).child("currentTripId").setValue("");
+        }
+
+        SharedPreferences.Editor prefsEditor = mPrefs.edit();
+        Gson gson = new Gson();
+        userData.setCurrentTripId("");
+        String json = gson.toJson(userData);
+        prefsEditor.putString("userData", json);
+        prefsEditor.putBoolean("isLogin", false);
+        prefsEditor.apply();
+
+        Intent intent = new Intent(getActivity(), MainActivity.class);
+        startActivity(intent);
+        getActivity().finish();
+
+    }
+    private void setPaymentAdapter(RecyclerView rvPayment) {
+        rvPayment.setLayoutManager(new LinearLayoutManager(requireContext()));
+        PaymentsAdapter adapter = new PaymentsAdapter(requireContext(), userDataArrayList);
+        rvPayment.setAdapter(adapter);
     }
 
     void setAdapter() {
@@ -47,11 +117,8 @@ public class ExpanseListFragment extends Fragment {
             Intent intent = new Intent(getActivity(), ExpansedetailActivity.class);
             intent.putExtra("data", expanseDataArrayList.get(index));
             startActivity(intent);
-//            ExpenseDetailFragment expenseDetailFragment = new ExpenseDetailFragment();
-//            expenseDetailFragment.setArguments(bundle);
-//            ((MainActivity) getActivity()).openFragment(expenseDetailFragment, true);
         });
         rvExpanse.setAdapter(adapter);
     }
-
 }
+
