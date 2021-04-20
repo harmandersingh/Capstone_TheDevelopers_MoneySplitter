338df0979dee80f95fd9878f50ab31373be834e9
Jimmy <harmander.singh.rai@gmail.com>
2021-04-21 00:07:51 +0530

    update signout

--- app/src/main/java/com/example/capstone_thedevelopers_moneysplitter/TripOptionFragment.java
+++ app/src/main/java/com/example/capstone_thedevelopers_moneysplitter/TripOptionFragment.java
@@ -1,15 +1,22 @@
 package com.example.capstone_thedevelopers_moneysplitter;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
+
 import androidx.fragment.app.Fragment;
+
 public class TripOptionFragment extends Fragment {
 
-    TextView btnAddTrip, btnViewTrip;
+    TextView btnAddTrip, btnViewTrip, txtWelcome;
+    SharedPreferences mPrefs;
+    private UserData userData = new UserData();
 
     public TripOptionFragment() {
         // Required empty public constructor
@@ -22,6 +29,12 @@ public class TripOptionFragment extends Fragment {
         // Inflate the layout for this fragment
         btnAddTrip = view.findViewById(R.id.btnAddTrip);
         btnViewTrip = view.findViewById(R.id.btnViewTrip);
+        txtWelcome = view.findViewById(R.id.txtWelcome);
+        mPrefs = getActivity().getSharedPreferences("MyPREFERENCES", Context.MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = mPrefs.getString("userData", "");
+        userData = gson.fromJson(json, UserData.class);
+        txtWelcome.setText("Welcome " + userData.getName());
 
         btnAddTrip.setOnClickListener(v -> ((MainActivity) getActivity()).openFragment(new AddTripFragment(), true));
         btnViewTrip.setOnClickListener(v -> ((MainActivity) getActivity()).openFragment(new ViewTripFragment(), true));
