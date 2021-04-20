d3d7cc28d968221d3b4e1999b3d8966a9bacbd03
DELL <raj749715@gmail.com>
2021-04-20 15:58:26 -0400

    add config file

---
+++ app/src/main/java/com/example/capstone_thedevelopers_moneysplitter/Config.java
@@ -0,0 +1,6 @@
+package com.example.capstone_thedevelopers_moneysplitter;
+
+public class Config {
+    public static final String PAYPAL_CLIENT_ID = "AcHLvI39VovkNFSSHKAWbyOcpS77xnUm0WTrUXo109LKr8cfQKK53XvB7BrcdQNi5nOzx5GVWuUCAuhX";
+
+}
