28b9cba24b3efcd410cd6f7ef862fa6d368f0012
DELL <raj749715@gmail.com>
2021-04-20 16:32:20 -0400

    update

--- app/src/main/java/com/example/capstone_thedevelopers_moneysplitter/ExpanseDetailActivity.java
+++ app/src/main/java/com/example/capstone_thedevelopers_moneysplitter/ExpanseDetailActivity.java
@@ -3,20 +3,25 @@ package com.example.capstone_thedevelopers_moneysplitter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class ExpanseDetailActivity extends AppCompatActivity implements OnMapReadyCallback {
     private GoogleMap mMap;
     ExpanseData expanseData = new ExpanseData();
-    private long lat,lng;
+    private double lat,lng;
     TextView txtName,txtAmount,txtDescription;
+    ImageView imgBill;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +30,7 @@ public class ExpanseDetailActivity extends AppCompatActivity implements OnMapRea
         txtName = findViewById(R.id.txtName);
         txtAmount = findViewById(R.id.txtAmount);
         txtDescription = findViewById(R.id.txtDescription);
+        imgBill = findViewById(R.id.imgBill);
         expanseData = (ExpanseData) getIntent().getSerializableExtra("data");
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -32,19 +38,34 @@ public class ExpanseDetailActivity extends AppCompatActivity implements OnMapRea
         txtName.setText(expanseData.getUserName());
         txtAmount.setText(expanseData.getExpanse());
         txtDescription.setText(expanseData.getDescription());
-//       lat = Long.parseLong(expanseData.getLat());
-//        lng = Long.parseLong(expanseData.getLng());
+        lat = Double.parseDouble(expanseData.getLat());
+        lng = Double.parseDouble(expanseData.getLng());
+        Log.d("", "");
+        Glide
+                .with(this)
+                .load(expanseData.getImage())
+                .centerCrop()
+                .into(imgBill);
+
 
     }
     @Override
     public void onMapReady(GoogleMap googleMap) {
 
 
-//        mMap = googleMap;
-//        LatLng latLng = new LatLng(lat, lng);
-//        mMap.addMarker(new
-//                MarkerOptions().position(latLng).title(""));
-//        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+       mMap = googleMap;
+        LatLng latLng = new LatLng(lat, lng);
+        mMap.addMarker(new
+                MarkerOptions().position(latLng).title(""));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        final CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(latLng)      // Sets the center of the map to Mountain View
+                .zoom(13)                   // Sets the zoom
+                .bearing(90)                // Sets the orientation of the camera to east
+                .tilt(30)                   // Sets the tilt of the camera to 30 degrees
+                .build();                   //
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
     }
 
    }
